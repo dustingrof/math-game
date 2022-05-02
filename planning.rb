819aec550c82f813f1c0599d2015276c49f1cf49
Dustin Grof <dustingrof@gmail.com>
2022-05-02 13:23:29 -0700

    initalize and add planning doc

---
+++ planning.rb
@@ -0,0 +1,31 @@
+class Game
+=begin
+Will keep track of which two players are playing. 
+
+Will keep track of who's turn it is
+
+Insert question and answer here.
+
+Will keep track of their scores. 
+
+Then will give a response once a user answers the question.
+
+Will print out score after each turn (method)
+=end
+end
+
+class Question
+  def initialize
+    # Will generate a new question and the answer. 
+    # Then will give a response once a user answers the question.
+    # Method to print out the question
+  end
+end
+
+class Player
+=begin
+The player will hold an id, name.
+Tell the game to update the score if wrong
+Could have the score
+=end
+end
